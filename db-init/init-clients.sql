13a169d842ce63a33a8939e7d933cdbc20c4be01
Vincent de Grandpré <vincent@de-grandpre.quebec>
2026-07-04 12:41:02 -0400

    service commandes

--- db-init/init-clients.sql
+++ db-init/init-clients.sql
@@ -1,5 +1,8 @@
+-- Importation de caractères accentués
+SET NAMES 'utf8mb4';
+
 -- Create database
-CREATE DATABASE IF NOT EXISTS bd_clients;
+CREATE DATABASE IF NOT EXISTS bd_clients CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE bd_clients;
 
 GRANT ALL PRIVILEGES ON bd_clients.* TO 'cantelcox'@'%';
@@ -9,7 +12,8 @@ DROP TABLE IF EXISTS party;
 CREATE TABLE party (
     id INT AUTO_INCREMENT PRIMARY KEY,
     uuid VARCHAR(150) NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     phoneNumber VARCHAR(15),
     city VARCHAR(75),
@@ -20,9 +24,10 @@ CREATE TABLE party (
     street2 VARCHAR(50),
     isOrganization BIT,
     username VARCHAR(75),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    partyStatus VARCHAR(25) DEFAULT 'ACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO party (name, uuid, email, username) VALUES
-('Vincent de Grandpré', UUID(), 'vincent.de-grandpre.1@ens.etsmtl.ca','operateur'),
-('Abonné', UUID(), 'test-cantelcox@de-grandpre.quebec','abonne');
+INSERT INTO party (firstName, lastName, uuid, email, username) VALUES
+('Vincent', 'de Grandpré', '2dd68aef-1868-46a9-ac0b-2c1f21a2c53d', 'vincent.de-grandpre.1@ens.etsmtl.ca','operateur'),
+('Abonné', 'CanTelcoX', '5e4e94c4-81c9-41f6-9935-782f6f88829c', 'test-cantelcox@de-grandpre.quebec','abonne');
--- db-init/init-commandes.sql
+++ db-init/init-commandes.sql
@@ -1,5 +1,8 @@
+-- Importation de caractères accentués
+SET NAMES 'utf8mb4';
+
 -- Create database
-CREATE DATABASE IF NOT EXISTS bd_commandes;
+CREATE DATABASE IF NOT EXISTS bd_commandes CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE bd_commandes;
 
 GRANT ALL PRIVILEGES ON bd_commandes.* TO 'cantelcox'@'%';
@@ -8,12 +11,16 @@ GRANT ALL PRIVILEGES ON bd_commandes.* TO 'cantelcox'@'%';
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    partyRef VARCHAR(150) NOT NULL,
-    total_amount DECIMAL(12,2) NOT NULL,
+    partyRef INT NOT NULL,
+    orderTotalPrice DECIMAL(12,2) NOT NULL,
+    cancellationDate DATETIME,
+    category VARCHAR(25),
     payment_link VARCHAR(100),
     is_paid BOOLEAN,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    completionDate DATETIME,
+    description VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Products table
 DROP TABLE IF EXISTS products;
@@ -23,8 +30,9 @@ CREATE TABLE products (
     productOfferingRef VARCHAR(150),
     price DECIMAL(10,2) NOT NULL,
     serviceRef VARCHAR(150),
+    type ENUM('produit','service'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Order items
 DROP TABLE IF EXISTS order_items;
@@ -33,13 +41,15 @@ CREATE TABLE order_items (
     order_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL DEFAULT 1,
-    unit_price DECIMAL(10,2) NOT NULL,
+    itemPrice DECIMAL(10,2) NOT NULL,
+    itemTotalPrice DECIMAL(10,2) NOT NULL,
+    description VARCHAR(255),
     FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE RESTRICT
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO products (name, productOfferingRef, serviceRef, price) VALUES
-('Samsung A54', 'SMGA54', NULL, 299.99),
-('Apple iPhone 8 - Promo', 'IPH008', NULL, 499.99),
-('Motorola XYZ', 'MOTXYZ', NULL, 20.99),
-('Ligne Mobile - Accès mensuel', 'MOB001', 'CanTelcoX-L1', 45.99);
+INSERT INTO products (name, productOfferingRef, serviceRef, price, type) VALUES
+('Samsung A54', 'SMGA54', NULL, 299.99, 'produit'),
+('Apple iPhone 8 - Promo', 'IPH008', NULL, 499.99, 'produit'),
+('Motorola XYZ', 'MOTXYZ', NULL, 20.99, 'produit'),
+('Ligne Mobile - Accès mensuel', 'MOB001', 'CanTelcoX-L1', 45.99, 'service');
