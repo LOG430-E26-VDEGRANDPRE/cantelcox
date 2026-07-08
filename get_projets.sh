35516ec1b22c41b3aaab8820b91aca070806d63f
Vincent de Grandpré <vincent@de-grandpre.quebec>
2026-07-08 11:46:05 -0400

    get_projets v2

--- get_projets.sh
+++ get_projets.sh
@@ -5,3 +5,11 @@ git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-clients.git
 git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-facturation.git
 git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-audit.git
 git clone https://github.com/LOG430-E26-VDEGRANDPRE/test-charge.git
+
+SERVICES=("svc-catalogue" "svc-clients" "svc-commandes" "svc-facturation" "svc-orchestration" "svc-lignes")
+
+for service in "${SERVICES[@]}"; do
+    cd $service
+    cp .env-example .env
+    cd ..
+done
