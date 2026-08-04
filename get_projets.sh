c31554d0753b656d6c7dc798f06026827d184e80
Miriam Brillon-Emond <miriam.brillon-emond.1@ens.etsmtl.ca>
2026-08-04 12:49:32 -0400

    added portability hub to deployment

--- get_projets.sh
+++ get_projets.sh
@@ -1,16 +1,70 @@
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-orchestration.git
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-commandes.git
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-lignes.git
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-catalogue.git
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-clients.git
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-facturation.git
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/svc-audit.git
-git clone https://github.com/LOG430-E26-VDEGRANDPRE/test-charge.git
 
-SERVICES=("svc-catalogue" "svc-clients" "svc-commandes" "svc-facturation" "svc-orchestration" "svc-lignes" "svc-audit")
+protocol="http"
+organisation="LOG430-E26-VDEGRANDPRE"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --http)
+            protocol="http"
+            shift
+            ;;
+        --ssh)
+            protocol="ssh"
+            shift
+            ;;
+        -o|--organisation)
+            organisation="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -*)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+DEFAULT_SERVICES=(
+    svc-catalogue
+    svc-clients
+    svc-commandes
+    svc-facturation
+    svc-orchestration
+    svc-lignes
+    svc-audit
+    svc-portability-hub
+    test-charge
+)
+
+# If no services were specified, clone all of them.
+if [[ $# -eq 0 ]]; then
+    SERVICES=("${DEFAULT_SERVICES[@]}")
+else
+    SERVICES=("$@")
+fi
 
 for service in "${SERVICES[@]}"; do
-    cd $service
-    cp .env.example .env
-    cd ..
+    if [[ "$protocol" == "http" ]]; then
+        repo_url="https://github.com/${organisation}/${service}.git"
+    else
+        repo_url="git@github.com:${organisation}/${service}.git"
+    fi
+
+    # echo "Cloning $repo_url"
+    git clone "$repo_url"
+done
+
+
+for service in "${SERVICES[@]}"; do
+    [[ "$service" == "svc-portability" ]] && continue
+
+    if [[ -f "$service/.env.example" ]]; then
+        cp "$service/.env.example" "$service/.env"
+    fi
 done
